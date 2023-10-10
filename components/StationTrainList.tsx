005d9226db6d763ead6f3a79381a5ddc034d690f
Anni Rikkonen <anni.rikkonen94@gmail.com>
2023-10-10 09:51:03 +0300

    comments to trainList

--- components/StationTrainList.tsx
+++ components/StationTrainList.tsx
@@ -17,7 +17,13 @@ const StationTrainList : React.FC<Props> = ({stationShort}: {stationShort : stri
         fetchTrains(stationShort);
     }, [])
 
+    //TrainListItem component shows relevant information about a train and takes TrainStopData as props.
+    //This function extracts required information from the Train object and forms a TrainStopData object from it.
     const formTrainStopData = (train : Train) : TrainStopData => {
+
+        //Displayed train name depends on train type.
+        //Only line id (R, U...) is shown for commuter trains.
+        //Long distance trains will show train type (IC, S...) and train number.
         let trainName : string = String(train.trainNumber);
 
         if(train.trainCategory === "Commuter" && train.commuterLineID?.length !== undefined) {
@@ -26,6 +32,8 @@ const StationTrainList : React.FC<Props> = ({stationShort}: {stationShort : stri
             trainName = `${train.trainType} ${train.trainNumber}`
         }
 
+        //Train object has a TimeTableRow object for every station it arrives to and departures from.
+        //Find method finds the relevant TimeTableRow for active station.
         let trainOnStation = train.timeTableRows.find((timeTable : TimeTableRow) => timeTable.stationShortCode === stationShort && timeTable.type === "DEPARTURE");
 
         return {
@@ -34,9 +42,8 @@ const StationTrainList : React.FC<Props> = ({stationShort}: {stationShort : stri
             cancelled: train.cancelled,
             stationTo: getStationName(train.timeTableRows[train.timeTableRows.length - 1].stationShortCode),
             stationFrom: getStationName(train.timeTableRows[0].stationShortCode),
-            timeTableRow: train.timeTableRows.find((timeTable : TimeTableRow) => 
-                                                    timeTable.stationShortCode === stationShort && timeTable.type === "DEPARTURE"
-                                                ) || {
+            //If data is somehow incoherrent and relevant TimeTableRow for active station is not found, a shallow TimeTableRow is created to avoid software errors.
+            timeTableRow: trainOnStation || {
                                                     trainStopping: true,
                                                     stationShortCode: "",
                                                     countryCode: "fi",
@@ -56,6 +63,8 @@ const StationTrainList : React.FC<Props> = ({stationShort}: {stationShort : stri
             ? <FlatList 
                 data={trains}
                 renderItem={({item}) => {return (<TrainListItem trainStopData={formTrainStopData(item)} />)}}
+                maxToRenderPerBatch={10}
+                keyExtractor={(item) => item.trainNumber + item.timeTableRows[0].scheduledTime}
                 />
             : <></>
         
